3cd12a32ee03a5f88d02ef9f088f94cb1fd6a040
Satvik Anand <satvikanand1205@gmail.com~>
2026-09-24 06:40:45 +0530

    feat:Added basic LLM Model using Langchain

---
+++ level4/index.js
@@ -0,0 +1,109 @@
+import express from "express"
+import dotenv from "dotenv"
+import { GoogleGenAI } from "@google/genai"
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai"
+import { ChatGroq } from "@langchain/groq"
+import { Annotation } from "@langchain/langgraph"
+
+dotenv.config()
+
+const port = 5000
+
+const app = express()
+app.use(express.json())
+
+
+// const ai = new GoogleGenAI({
+//     apiKey:process.env.GEMINI_API_KEY
+// })
+
+// app.post("/ai", async (req, res) => {
+//     const { input } = req.body
+
+//        const response = await ai.models.generateContent({
+//         model: "gemini-3.5-flash",
+//         contents: [
+//             {
+//                 role: "system",
+//                 parts: [{ text: "You are assistant and your name is Jarvis" }]
+//             },
+//             {
+//                 role: "user",
+//                 parts: [{ text: input }]
+//             }
+//         ]
+//     })
+
+//     return res.status(200).json({ ai: response.text })
+// })
+
+
+//WITH LANGCHAIN
+
+// const llm = new ChatGoogleGenerativeAI({
+//     model:"gemini-3.5-flash"
+// })
+
+//GROQ
+
+const llm = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature:0.7,
+    maxTokens:100,
+    maxRetries:2
+})
+
+const State = Annotation.Root({
+    prompt:Annotation,
+    aiMsg:Annotation
+})
+
+const callLLm = async (state)=>{
+    console.log("state:",state)
+
+    const response = await llm.invoke([
+        {
+            role:"system",
+            content:"You are assistant and your name is Jarvis. If you don't know the answer please don't give the wrong answer"
+        },
+        {
+            role:"human",
+            content:state.prompt
+        }
+    ])
+
+    return {aiMsg:response.content}
+}
+
+const graph = new StateGraph(State)
+.addNode("agent",callLLm)
+.addEdge("_start_","agent")
+.addEdge("agent","_end_")
+.compile()
+
+
+// app.post("/ai",async(req,res)=>{
+
+//     const{input} = req.body
+
+//     const response = await llm.invoke([
+//         {
+//             role:"system",
+//             content:"You are assistant and your name is Jarvis. If you don't know the answer please don't give the wrong answer"
+//         },
+//         {
+//             role:"human",
+//             content:input
+//         }
+//     ])
+
+//     return res.status(200).json({"ai:":response.content})
+// })
+
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Hello from AI world"})
+})
+
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`)
+})
